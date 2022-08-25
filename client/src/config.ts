6da2965ce82cf139e99c1bd8619ec196741bfc68
Nguyen Dinh Phu <dinhphu951998@gmail.com>
2022-08-26 00:32:12 +0700

    authentication

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,13 @@
+
 export const apiId = process.env.REACT_APP_API_ID
 export const stage = process.env.REACT_APP_STAGE
 
 console.log(process.env)
 
 export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/${stage}`
+
+export const authConfig = {
+  domain: 'dev-mp9ket6w.us.auth0.com',
+  clientId: '8LnhRxZ0IEmLbJhxb8Q3eGBUkdTo21fl',
+  callbackUrl: 'http://localhost:3000/callback'
+}
